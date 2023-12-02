b24bdb43d0d845fc19b42a1b1987da739d5b1682
bielvrs <gabrielvrs031@gmail.com>
2023-12-01 22:22:09 -0300

    exercícos 002

---
+++ exercício 002/app.js
@@ -0,0 +1,5 @@
+function mostrarnumero(){
+    let numero = document.getElementById("numero").value
+    document.getElementById("resposta").innerText = "o número informado foi: " + numero
+    document.getElementById("numero").value = ""
+}
